9ce75715516ecacb27274bda7f3517d88a7b5288
Andrii Bugaiov <andriibugaiov@gmail.com>
2016-05-07 12:18:54 -0400

    chain of responsibility

--- ChainOfResponsibility.h
+++ ChainOfResponsibility.h
@@ -13,15 +13,17 @@ namespace ChainOfResponsibility {
 		}
 		virtual ~Role() {
 		}
-		const std::string getSpec() {
+		const std::string &getSpec() const {
 			return spec;
 		}
 	};
 	class Human {
-	public:
-		// TODO:
 		std::shared_ptr<Role> pRole;
-		Human(std::shared_ptr<Role> ppRole) : pRole(ppRole) {
+	public:
+		Human(std::unique_ptr<Role> &ppRole) : pRole(std::move(ppRole)) {
+		}
+		std::shared_ptr<const Role> getRole() {
+			return pRole;
 		}
 	};
 
@@ -32,30 +34,30 @@ namespace ChainOfResponsibility {
 	};
 	class Handler {
 	protected:
-		std::weak_ptr<Handler> pNext;
+		std::weak_ptr<const Handler> pNext;
 	public:
-		Handler(std::weak_ptr<Handler> pHandler) : pNext(pHandler) {
+		Handler(std::weak_ptr<const Handler> pHandler) : pNext(pHandler) {
 		}
-		virtual bool handle(const std::unique_ptr<Human> &pPnt) {
-			if (std::shared_ptr<Handler> ppNext = pNext.lock())
+		virtual ~Handler() {
+		}
+		virtual bool handle(const std::unique_ptr<Human> &pPnt) const {
+			if (std::shared_ptr<const Handler> ppNext = pNext.lock())
 				return ppNext -> handle(pPnt);
 			return false;
 		}	
 	};
 	class Doctor : public Role, public Handler {
 	public:
-		Doctor(const std::string &anExpertise, std::weak_ptr<Doctor> pNextDtr) : Role(anExpertise), Handler(pNextDtr) {
+		Doctor(const std::string &anExpertise, std::weak_ptr<const Doctor> pNextDtr) : Role(anExpertise), Handler(pNextDtr) {
 		}
-		// virtual ~Doctor() {
-		// 	std::unique_ptr<Doctor> pNextDtr(static_cast<Doctor *>(pNext.release()));
-		// }
-		virtual bool handle(const std::unique_ptr<Human> &pPnt) {
+		virtual bool handle(const std::unique_ptr<Human> &pPnt) const {
 			bool isHandled = false;
 			std::cout << "Doctor [" << getSpec() << "] is examining..." << std::endl;
 
-			std::shared_ptr<Patient> pRole = std::dynamic_pointer_cast<Patient>(pPnt -> pRole);
+			std::shared_ptr<const Role> ppRole = pPnt -> getRole();
+			std::shared_ptr<const Patient> pRole = std::dynamic_pointer_cast<const Patient>(ppRole);
 			if (getSpec().find(pRole -> getSpec()) != std::string::npos) {
-				std::cout << "Diagnosed." << std::endl;
+				std::cout << "Diagnosed by [" << getSpec() << "]" << std::endl;
 				isHandled = true;
 			}
 
@@ -69,43 +71,45 @@ namespace ChainOfResponsibility {
 		std::list<std::unique_ptr<Human>> pDtrs;
 	public:
 		Hospital() {
-			std::list<std::string> expertises =  {
+			std::vector<std::string> expertises =  {
 				"Dentists: tooth", 
 				"Dermatologists‎: skin",
 				"Cardiologists‎: heart",
 				"Psychiatrists: mind"
 			};
 			for (auto &expertise : expertises) {
-				std::shared_ptr<Doctor> ppRole;
-				if (!pDtrs.empty())
-					ppRole = std::dynamic_pointer_cast<Doctor>(pDtrs.front() -> pRole);
+				std::shared_ptr<const Doctor> ppRole;
+				if (!pDtrs.empty()) {
+					std::shared_ptr<const Role> pppRole = pDtrs.front() -> getRole();
+					ppRole = std::dynamic_pointer_cast<const Doctor>(pppRole);
+				}
 
-				// TODO:
-				std::shared_ptr<Doctor> pRole(new Doctor(expertise, ppRole));
+				std::unique_ptr<Role> pRole(new Doctor(expertise, ppRole));
 				std::unique_ptr<Human> pDtr(new Human(pRole));
 
 				pDtrs.push_front(std::move(pDtr));
 			}
 		}
 		void examine(const std::unique_ptr<Human> &pPnt) {
-			std::shared_ptr<Doctor> ppRole;
-			if (!pDtrs.empty())
-				ppRole = std::dynamic_pointer_cast<Doctor>(pDtrs.front() -> pRole);
-			if (ppRole)
-				ppRole -> handle(pPnt);
+			if (!pDtrs.empty()) {
+				std::shared_ptr<const Patient> pRole = std::dynamic_pointer_cast<const Patient>(pPnt -> getRole());
+				std::cout << "Examining patient with complait [" << pRole -> getSpec() << "]..." << std::endl;
+
+				std::shared_ptr<const Role> ppRole = pDtrs.front() -> getRole();
+				std::shared_ptr<const Doctor> pppRole = std::dynamic_pointer_cast<const Doctor>(ppRole);
+				pppRole -> handle(pPnt);
+			}
 		}
 	};
 
 	void TestSuite() {
 		std::unique_ptr<Hospital> pHospital(new Hospital());
-
-		// TODO:
-		std::shared_ptr<Patient> pRole1(new Patient("heart"));
+		
+		std::unique_ptr<Role> pRole1(new Patient("heart"));
 		std::unique_ptr<Human> pPatient1(new Human(pRole1));
 		pHospital -> examine(pPatient1);
 
-		// TODO:
-		std::shared_ptr<Patient> pRole2(new Patient("skin"));
+		std::unique_ptr<Role> pRole2(new Patient("skin"));
 		std::unique_ptr<Human> pPatient2(new Human(pRole2));
 		pHospital -> examine(pPatient2);
 	}
